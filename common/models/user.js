729c36f271ac9b6ae3eef30db644b2648da28dce
Samuel Mussie <punisami@gmail.com>
2018-12-21 21:53:36 +0300

    added include user, changed port to 5000

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(User) {
-
+  User.afterRemote('login', (context, remoteMethodOutput, next) => {
+    User.findOne({
+      where: {
+        id: remoteMethodOutput.userId,
+      },
+    },
+    function(err, user) {
+      remoteMethodOutput.user = user;
+      next();
+    }
+  );
+  });
 };
